63c6b3a34056204292664067b38ece3b34301785
Rohit Wagh <waghrohit483@gmail.com>
2025-06-13 16:53:03 +0530

    Create SG.tf

---
+++ sf.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = "vpc-0edba9c067b5bb9ea"
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+# ingres = inbound rule
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+
+#egree = outbound rules 
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
